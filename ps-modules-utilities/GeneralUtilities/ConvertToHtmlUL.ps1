b5cd5d99232d25779997b6a23a4877984f8af091
Rod Meaney <roderick.meaney@coles.com.au>
2024-01-04 09:28:24 +1100

    useful general utilities

---
+++ ps-modules-utilities/GeneralUtilities/ConvertToHtmlUL.ps1
@@ -0,0 +1,32 @@
+﻿function ConvertTo-HtmlUL{
+    <#
+    .SYNOPSIS
+        Convert String array to the HTML for a UL
+
+    .DESCRIPTION
+        Convert String array to the HTML for a UL
+
+    .INPUTS
+        No piping for the cmd-let.
+
+    .OUTPUTS
+        The HTML for a UL
+
+    .EXAMPLE
+        Example of how to run the script.
+
+    .LINK
+        n/a
+
+    .NOTES
+        n/a
+
+    #>
+[CmdletBinding()]
+param (
+    #Array of text items
+    [Parameter(Mandatory=$true)] [String[]]$StrList
+)
+    Return "<ul>" + (($StrList | foreach-object{"<li>$_</li>"}) -join "") + "</ul>"
+}
+Export-ModuleMember -Function ConvertTo-HtmlUL
